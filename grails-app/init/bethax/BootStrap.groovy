9250fbecead06836e208b568edaa49736fd977b3
Pau Balaguer <pbalaguer19@gmail.com>
2017-11-09 16:36:36 +0100

    IRB Research Groups added to BootStrap.groovy. #3

--- grails-app/init/bethax/BootStrap.groovy
+++ grails-app/init/bethax/BootStrap.groovy
@@ -2,6 +2,7 @@ package bethax
 
 import grails.converters.JSON
 import org.irblleida.bethax.Institution
+import org.irblleida.bethax.InstitutionSection
 import org.irblleida.bethax.InstitutionType
 import org.irblleida.bethax.Organization
 import org.irblleida.bethax.Role
@@ -72,8 +73,39 @@ class BootStrap {
 
         new Institution(createdBy: pau, lastModifiedBy: pau, name: "GOC", institutionType: InstitutionType.PRIVATE_EXTERNAL).save(flush: true)
 
-
-
+        def irb = Institution.findByName("IRBLleida")
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Altres línies de recerca").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Biologia de Sistemes i Mètodes Estadístics per a la Recerca Mèdica").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Biologia Molecular de Llevats").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Bioquímica de l'Estrès Oxidatiu").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Cicle Cel·lular").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Cirurgia Endocrina").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Cirurgia Experimental").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Desenvolupament i Evolució del Cervell").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Epidemiologia Aplicada").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Farmacoepidemiologia").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Fisiopatologia Metabòlica").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Fonaments Biològics dels Trastorns Mentals").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Genètica de Malalties Complexes").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "GRECS - Grup de Recerca de Cures de Salut").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Grup de Recerca en Immunologia i Metabolisme").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Grup de Recerca de Biomarcadors en Càncer (GReBiC)").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Recerca Translacional en Medicina Respiratòria").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Medicina Intensiva").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Neurobiologia Molecular i del Desenvolupament").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Clinical Neurosciences").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Neurocognició, psicobiologia de la personalitat i genètica de la conducta").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Neuroimmunologia").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Patologia Neuromuscular Experimental").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Patologia Oncològica").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Recerca Clínica i Experimental en Patologia Digestiva i Hematològica").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Senyalització Cel·lular i Apoptosi").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Senyalización en Levaduras").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Senyalització Oncogènica I Del Desenvolupament").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Senyalització Cel·lular per Calci").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Unitat de Farmacologia").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Unitat de Senyalització Neuronal").save(flush: true)
+        new InstitutionSection(createdBy: pau, lastModifiedBy: pau, institution: irb, name: "Vascular and Renal Translational Research Group").save(flush: true)
     }
 
     def destroy = {
